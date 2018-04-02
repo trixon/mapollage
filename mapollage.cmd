d435c69bae2be00c2215a94dc48b90e01ccc84b6
Patrik Karlström <patrik@trixon.se>
2018-04-02 11:18:53 +0200

    Add Windows cmd file

---
+++ mapollage.cmd
@@ -0,0 +1,9 @@
+REM This file is intended for Windows users who wish to start
+REM the program in text mode from a console window.
+REM
+REM This is because how the exe wrapper launch4j handles header types.
+REM
+REM If java is not in your PATH, make it so, or change the
+REM java part of the command below to include the full path.
+REM
+@java -jar mapollage.exe %*
