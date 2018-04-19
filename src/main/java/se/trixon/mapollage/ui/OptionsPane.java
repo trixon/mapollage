6114786d425c78c113437a03a0779a85cfff1598
Patrik Karlström <patrik@trixon.se>
2018-04-19 14:37:38 +0200

    Implement options pane load/save

--- src/main/java/se/trixon/mapollage/ui/OptionsPane.java
+++ src/main/java/se/trixon/mapollage/ui/OptionsPane.java
@@ -15,11 +15,17 @@
  */
 package se.trixon.mapollage.ui;
 
+import java.util.ResourceBundle;
 import javafx.geometry.Insets;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import se.trixon.almond.util.Dict;
+import se.trixon.almond.util.SystemHelper;
 import se.trixon.almond.util.fx.control.LocaleComboBox;
 import se.trixon.mapollage.Options;
 
@@ -29,33 +35,121 @@ import se.trixon.mapollage.Options;
  */
 public class OptionsPane extends GridPane {
 
-    private LocaleComboBox mLocaleComboBox;
-
+    private final ResourceBundle mBundle = SystemHelper.getBundle(OptionsPane.class, "Bundle");
+    private final CheckBox mCleanNs2CheckBox = new CheckBox(mBundle.getString("OptionsPanel.cleanNs2CheckBox.text"));
+    private final CheckBox mCleanSpaceCheckBox = new CheckBox(mBundle.getString("OptionsPanel.cleanSpaceCheckBox.text"));
+    private final Font mDefaultFont = Font.getDefault();
+    private final Spinner<Double> mDefaultLatitudeSpinner = new Spinner(-180, 180, 0, 0.01);
+    private final Spinner<Double> mDefaultLongitudeSpinner = new Spinner(-90, 90, 0, 0.01);
+    private final LocaleComboBox mLocaleComboBox = new LocaleComboBox();
+    private final CheckBox mLogKmlCheckBox = new CheckBox(mBundle.getString("OptionsPanel.logKmlCheckBox.text"));
     private final Options mOptions = Options.getInstance();
-    private CheckBox mWordWrapCheckBox;
+    private final Spinner<Integer> mThumbnailBorderSizeSpinner = new Spinner(0, 20, 2, 1);
+    private final Spinner<Integer> mThumbnailSizeSpinner = new Spinner(100, 1200, 250, 10);
+    private final Insets mTopInsets = new Insets(8, 0, 0, 0);
+    private final CheckBox mWordWrapCheckBox = new CheckBox(Dict.DYNAMIC_WORD_WRAP.toString());
 
     public OptionsPane() {
         createUI();
         load();
     }
 
+    private void addTopMargin(Region... regions) {
+        for (Region region : regions) {
+            GridPane.setMargin(region, mTopInsets);
+        }
+    }
+
+    private void addTopPadding(Region... regions) {
+        for (Region region : regions) {
+            region.setPadding(mTopInsets);
+        }
+    }
+
     private void createUI() {
-        Label label = new Label(Dict.CALENDAR_LANGUAGE.toString());
-        mLocaleComboBox = new LocaleComboBox();
-        mWordWrapCheckBox = new CheckBox(Dict.DYNAMIC_WORD_WRAP.toString());
-        setGridLinesVisible(true);
-        addColumn(0, label, mLocaleComboBox, mWordWrapCheckBox);
-        GridPane.setMargin(mWordWrapCheckBox, new Insets(16, 0, 0, 0));
+        //setGridLinesVisible(true);
+        String fontFamily = mDefaultFont.getFamily();
+        double fontSize = mDefaultFont.getSize();
+
+        Font font = Font.font(fontFamily, FontPosture.ITALIC, fontSize * 1.3);
+
+        Label calendarLanguageLabel = new Label(Dict.CALENDAR_LANGUAGE.toString());
+        Label placemarkLabel = new Label(Dict.PLACEMARK.toString());
+        Label logLabel = new Label(Dict.LOG.toString());
+        Label thumbnailLabel = new Label(Dict.THUMBNAIL.toString());
+        Label latitudeLabel = new Label(Dict.LATITUDE.toString());
+        Label longitudeLabel = new Label(Dict.LONGITUDE.toString());
+        Label borderSizeLabel = new Label(mBundle.getString("OptionsPanel.borderSizeLabel.text"));
+        Label cleanLabel = new Label(mBundle.getString("OptionsPanel.cleanLabel.text"));
+        Label defaultCoordinateLabel = new Label(mBundle.getString("OptionsPanel.coordinateLabel.text"));
+
+        placemarkLabel.setFont(font);
+        defaultCoordinateLabel.setFont(font);
+        cleanLabel.setFont(font);
+        logLabel.setFont(font);
+
+        mDefaultLongitudeSpinner.setEditable(true);
+        mDefaultLatitudeSpinner.setEditable(true);
+        mThumbnailSizeSpinner.setEditable(true);
+        mThumbnailBorderSizeSpinner.setEditable(true);
+
+        addColumn(0,
+                calendarLanguageLabel,
+                mLocaleComboBox,
+                placemarkLabel,
+                thumbnailLabel,
+                mThumbnailSizeSpinner,
+                borderSizeLabel,
+                mThumbnailBorderSizeSpinner,
+                defaultCoordinateLabel,
+                latitudeLabel,
+                mDefaultLatitudeSpinner,
+                longitudeLabel,
+                mDefaultLongitudeSpinner,
+                cleanLabel,
+                mCleanNs2CheckBox,
+                mCleanSpaceCheckBox,
+                logLabel,
+                mWordWrapCheckBox,
+                mLogKmlCheckBox
+        );
+
+        addTopPadding(
+                placemarkLabel,
+                defaultCoordinateLabel,
+                borderSizeLabel,
+                longitudeLabel,
+                cleanLabel,
+                logLabel
+        );
+
+        addTopMargin(
+                mCleanSpaceCheckBox,
+                mLogKmlCheckBox
+        );
     }
 
     private void load() {
         mLocaleComboBox.setLocale(mOptions.getLocale());
         mWordWrapCheckBox.setSelected(mOptions.isWordWrap());
+        mCleanNs2CheckBox.setSelected(mOptions.isCleanNs2());
+        mCleanSpaceCheckBox.setSelected(mOptions.isCleanSpace());
+        mDefaultLatitudeSpinner.getValueFactory().setValue(mOptions.getDefaultLat());
+        mDefaultLongitudeSpinner.getValueFactory().setValue(mOptions.getDefaultLon());
+        mLogKmlCheckBox.setSelected(mOptions.isLogKml());
+        mThumbnailSizeSpinner.getValueFactory().setValue(mOptions.getThumbnailSize());
+        mThumbnailBorderSizeSpinner.getValueFactory().setValue(mOptions.getThumbnailBorderSize());
     }
 
     void save() {
         mOptions.setLocale(mLocaleComboBox.getLocale());
         mOptions.setWordWrap(mWordWrapCheckBox.isSelected());
+        mOptions.setCleanNs2(mCleanNs2CheckBox.isSelected());
+        mOptions.setCleanSpace(mCleanSpaceCheckBox.isSelected());
+        mOptions.setDefaultLat(mDefaultLatitudeSpinner.getValue());
+        mOptions.setDefaultLon(mDefaultLongitudeSpinner.getValue());
+        mOptions.setLogKml(mLogKmlCheckBox.isSelected());
+        mOptions.setThumbnailSize(mThumbnailSizeSpinner.getValue());
+        mOptions.setThumbnailBorderSize(mThumbnailBorderSizeSpinner.getValue());
     }
-
 }
